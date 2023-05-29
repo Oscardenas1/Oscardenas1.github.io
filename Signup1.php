cc38a0cf16ff76abba229998b76b0d9e6a0d594e
Oscar Cardenas <oscarfish1000@gmail.com>
2023-05-29 16:46:48 -0600

    new changes

--- Signup1.php
+++ Signup1.php
@@ -1,11 +1,5 @@
 <?php
-// Establish a MySQL connection
-$servername = "132.145.200.86";
-$username = "root";
-$password = "@Mysqlse2023";
-$dbname = "user_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+require_once 'conn.php';
 
 // Check connection
 if ($conn->connect_error) {
---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+
+// Establish a MySQL connection
+$servername = "132.145.200.86";
+$username = "root";
+$password = "@Mysqlse2023";
+$dbname = "user_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
